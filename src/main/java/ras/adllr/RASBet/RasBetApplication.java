f9fd12338e912c0896c09c1d0816c57713f9adaf
RayMightBeWrong <ruizinho7braga@gmail.com>
2022-10-08 15:55:08 +0100

    Springboot App

---
+++ src/main/java/ras/adllr/RASBet/RasBetApplication.java
@@ -0,0 +1,13 @@
+package ras.adllr.RASBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasBetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ras/adllr/RASBet/RasBetApplicationTests.java
@@ -0,0 +1,13 @@
+package ras.adllr.RASBet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
