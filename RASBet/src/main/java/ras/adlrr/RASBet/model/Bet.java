d9e7dc23a510d33ec4a264e021ff9a0bd1b43359
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2022-10-14 20:03:21 +0100

    Bet extends Transaction + Parte do diagrama de classes

--- RASBet/src/main/java/ras/adlrr/RASBet/model/Bet.java
+++ RASBet/src/main/java/ras/adlrr/RASBet/model/Bet.java
@@ -1,12 +1,15 @@
 package ras.adlrr.RASBet.model;
 
-public class Bet {
+import java.time.LocalDateTime;
+
+public class Bet extends Transaction{
     private int id;
     private boolean is_multiple;
     private int state;
     private int game_id;
 
-    public Bet(int id, boolean is_multiple, int state, int game_id) {
+    public Bet(int Transaction_id, double balance_after_mov, String description, float value, LocalDateTime date, int wallet_id, int gambler_id, int id, boolean is_multiple, int state, int game_id) {
+        super(id, balance_after_mov, description, value, date, wallet_id, gambler_id);
         this.id = id;
         this.is_multiple = is_multiple;
         this.state = state;
