ca80f0ab5f728fb351e00e4028daa467b47cd64a
RayMightBeWrong <ruizinho7braga@gmail.com>
2022-11-01 21:07:42 +0000

    Estruturar UcrasAPIReader

--- RASBet/src/main/java/ras/adlrr/RASBet/model/readers/UcrasAPIReader.java
+++ RASBet/src/main/java/ras/adlrr/RASBet/model/readers/UcrasAPIReader.java
@@ -5,8 +5,10 @@ import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
@@ -21,55 +23,81 @@ public class UcrasAPIReader implements APIGameReader{
         this.sport_id = sport_id;
     }
 
+    public String getGameExternalId(JSONObject jo){
+        return (String) jo.get("id");
+    }
+
+    public LocalDateTime getGameDate(JSONObject jo) {
+        String iso8601 = (String) jo.get("commenceTime");
+        ZonedDateTime zdt = ZonedDateTime.parse(iso8601);
+        LocalDateTime ldt = zdt.toLocalDateTime();
+
+        return ldt;
+    }
+
+    public Set<Participant> getGameParticipants(JSONObject jo){
+        String homeTeam = (String) jo.get("homeTeam");
+        String awayTeam = (String) jo.get("awayTeam");
+
+        JSONArray bookmakers = (JSONArray) jo.get("bookmakers");
+        JSONObject fst = (JSONObject) bookmakers.get(0);
+        JSONArray markets = (JSONArray) fst.get("markets");
+        JSONObject markets2 = (JSONObject) markets.get(0);
+        JSONArray outcomes = (JSONArray) markets2.get("outcomes");
+
+        float drawOdd = 0, homeOdd = 0, awayOdd = 0;
+        for (int j = 0; j < outcomes.length(); j++){
+            JSONObject obj = (JSONObject) outcomes.get(j);
+            Number odd = (Number) obj.get("price");
+            if (homeTeam.equals(obj.get("name"))){
+                homeOdd = odd.floatValue();
+            }
+            else if (awayTeam.equals(obj.get("name"))){
+                awayOdd = odd.floatValue();
+            }
+            else if (obj.get("name").equals("Draw")){
+                drawOdd = odd.floatValue();
+            }
+        }
+        Participant home = new Participant(homeTeam, homeOdd, 0);
+        Participant away = new Participant(awayTeam, awayOdd, 0);
+        Participant draw = new Participant("draw", drawOdd, 0);
+
+
+        Set<Participant> ps = new HashSet<>();
+        ps.add(home); ps.add(away); ps.add(draw);
+
+        return ps;
+    }
+
+    public int getGameState(){
+        return Game.OPEN;
+    }
+
+    public int getSportID(){
+        return this.sport_id;
+    }
+
+    public String makeName(JSONObject jo){
+        String homeTeam = (String) jo.get("homeTeam");
+        String awayTeam = (String) jo.get("awayTeam");
+
+        return homeTeam + " vs " + awayTeam;
+    }
+
     public List<Game> getAPIGames(){
         String json = readJSONfromHTTPRequest("http://ucras.di.uminho.pt/v1/games/");
+
+        List<Game> res = new ArrayList<>();
 
         JSONArray ja = new JSONArray(json);
         for(int i = 0; i < ja.length(); i++){
             JSONObject jo = (JSONObject) ja.get(i);
-
-            String iso8601 = (String) jo.get("commenceTime");
-            ZonedDateTime zdt = ZonedDateTime.parse(iso8601);
-            LocalDateTime ldt = zdt.toLocalDateTime();
-
-            String homeTeam = (String) jo.get("homeTeam");
-            String awayTeam = (String) jo.get("awayTeam");
-
-
-            JSONArray bookmakers = (JSONArray) jo.get("bookmakers");
-            JSONObject fst = (JSONObject) bookmakers.get(0);
-            JSONArray markets = (JSONArray) fst.get("markets");
-            JSONObject markets2 = (JSONObject) markets.get(0);
-            JSONArray outcomes = (JSONArray) markets2.get("outcomes");
-
-            float drawOdd = 0, homeOdd = 0, awayOdd = 0;
-            for (int j = 0; j < outcomes.length(); j++){
-                JSONObject obj = (JSONObject) outcomes.get(j);
-                Number odd = (Number) obj.get("price");
-                if (homeTeam.equals(obj.get("name"))){
-                    homeOdd = odd.floatValue();
-                }
-                else if (awayTeam.equals(obj.get("name"))){
-                    awayOdd = odd.floatValue();
-                }
-                else if (obj.get("name").equals("Draw")){
-                    drawOdd = odd.floatValue();
-                }
-            }
-            Participant home = new Participant(homeTeam, homeOdd, 0);
-            Participant away = new Participant(awayTeam, awayOdd, 0);
-            Participant draw = new Participant("draw", drawOdd, 0);
-            List<Participant> ps = new ArrayList<>();
-            ps.add(home); ps.add(away); ps.add(draw);
+            Game g = new Game(getGameExternalId(jo), getGameDate(jo), getGameState(), makeName(jo), getSportID(), getGameParticipants(jo));
+            res.add(g);
         }
 
-        return null;
-
-            // TODO: mudar para diferentes desportos e mudar o resultado
-            // TODO: get ID do desporto
-            //Game g = new Game(5, (String) jo.get("id"), ldt, ps, 1, Game.CLOSED);
-            //TODO
-            //gameRepository.addGame(g);
+        return res;
     }
 
     public String readJSONfromHTTPRequest(String urlString){
