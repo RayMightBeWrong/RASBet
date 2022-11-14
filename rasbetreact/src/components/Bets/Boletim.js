3e9159db95699a11e997be54cf7f48a3c86d54a0
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2022-11-14 21:58:34 +0000

    Change Boletim.js by Luis

--- rasbetreact/src/components/Bets/Boletim.js
+++ rasbetreact/src/components/Bets/Boletim.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Boletim.css';
 import { Button } from '../Button';
 import { BoletimBet } from './BoletimBet';
@@ -7,6 +7,15 @@ import { BoletimBet } from './BoletimBet';
 export const Boletim = ({
     bets
 }) => {
+    const [value, setValue] = useState('');
+
+    const handleChange = event => {
+        const result = event.target.value.replace(/\D/g, '');
+
+        setValue(result);
+    };
+
+
     return (
         <>
             <div className='boletim'>
@@ -17,8 +26,14 @@ export const Boletim = ({
                 </div>
                 <div>
                     {bets.map(game => (
-                      <div><BoletimBet title={game.title} winner={game.winner} cota={game.cota}/></div>
+                        <div><BoletimBet title={game.title} winner={game.winner} cota={game.cota} /></div>
                     ))}
+                </div>
+                <div>
+                    <input type="text" placeholder="Valor da aposta" name="aposta" required />
+                    <input type="text"
+                        value={value}
+                        onChange={handleChange} />
                 </div>
             </div>
         </>
